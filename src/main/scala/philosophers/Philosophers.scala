00e410daaed5955ca1c7a542ef98514916b555ff
kevin mc inerney <kevinmcinerney8@gmail.com>
2017-04-10 18:03:12 +0100

    use schema option with reader to cast straight to dataset

--- src/main/scala/philosophers/Philosophers.scala
+++ src/main/scala/philosophers/Philosophers.scala
@@ -35,92 +35,59 @@ object Philosophers {
   /** Main function */
   def main(args: Array[String]): Unit = {
     runProgram()
-
-    /*val rdd = spark.read.option("header","true").format("csv").load(fsPath("/philosophers/philosophers.csv"))
-
-
-    val dff = rdd.toDF("name", "country", "rating")
-
-    dff.show()
-
-    dff.printSchema()
-
-    dff.createOrReplaceTempView("philosophers")
-
-    val html = scala.io.Source.fromURL("https://www.oddschecker.com/horse-racing/naas").mkString("")
-    html.foreach(l => print(l))
-    //val rdds = spark.sparkContext.parallelize(list)
-    //rdds.toDF().first().get(0)*/
-
   }
 
   def runProgram(): Unit  = {
-
-    val (headings, df) = read("/philosophers/philosophers.csv")
 
     /**
       * Note that I can the below code, instead of the above. As long as
       * I comment comment out countrySummaryTyped and use the df passed
       * in instead of the typed, generated ds
       */
-    import spark.implicits._
+
+    val ds = spark
+      .read
+      .option("header","true")
+      .format("csv")
+      .schema(dfSchema(List("name","country","rating")))
+      .load(fsPath("/philosophers/philosophers.csv"))
+      .as[PhilosopherRow]
 
 
     println("Rating > 5 works with no schema and can be applied to string col")
-    df.printSchema()
-    df.filter($"rating" > 5).show()
+    ds.printSchema()
+    ds.filter($"rating" > 5).show()
 
-
-
-    val avgPhilRatingByCountry = ratingsByCountry(df)
+    val avgPhilRatingByCountry = ratingsByCountry(ds.select($"country", $"rating").as[CountryRow])
 
     avgPhilRatingByCountry.show()
 
-    val philDs = philosopherSummaryTyped(df)
-
-    philDs.show()
-
-    val descPhilByCountry = descPhilosopherByCountry(df)
+    println("Desc")
+    val descPhilByCountry =
+      ds
+        .groupBy('country)
+        .agg(
+          min('rating).as("minRating"),
+          max('rating).as("maxRating"),
+          max('rating).minus(min('rating)).as("range")
+        ).as[Descriptive]
 
     descPhilByCountry.show()
+
+    //Becasue of as[PhilosopherRow] I can access the filed .country nice!
+    descPhilByCountry.collect().head.country
+
 
     descPhilByCountry.select(($"minRating" + $"maxRating").as("added")).show()
 
 
     println("Rating > 5")
-    df.filter($"rating" > 5).show()
+    ds.filter($"rating" > 5).show()
 
     println("Rating > 5 again")
-    df.filter(df("rating") > 5).show()
-
-   // df.agg()
-
+    ds.filter(ds("rating") > 5).show()
 
   }
-
-
-  /** @return The read DataFrame along with its column names. */
-  def read(resource: String): (List[String], DataFrame) = {
-
-    val rdd = spark.sparkContext.textFile(fsPath(resource))
-
-    val headerColumns = rdd.first().split(",").to[List]
-
-    // Compute the schema based on the first line of the CSV file
-    val schema = dfSchema(headerColumns)
-
-    val data =
-      rdd
-        .mapPartitionsWithIndex((i, it) => if (i == 0) it.drop(1) else it) // skip the header line
-        .map(_.split(",").to[List])
-        .map(row)
-
-    val dataFrame =
-      spark.createDataFrame(data, schema)
-
-    (headerColumns, dataFrame)
-  }
-
 
 
   /** @return The filesystem path of the given resource */
@@ -129,8 +96,6 @@ object Philosophers {
     //println(path)
     path
   }
-
-
 
 
   /** @return The schema of the DataFrame, assuming that the first given column has type String and all the others
@@ -143,50 +108,18 @@ object Philosophers {
       columnNames.init.map(col => {
         StructField(col, StringType, nullable = false)
       }) :+
-       StructField(columnNames.last, IntegerType, nullable = false)
+        StructField(columnNames.last, DoubleType, nullable = false)
 
     )
-
   }
 
-
-  /** @return An RDD Row compatible with the schema produced by `dfSchema`
-    * @param line Raw fields
-    */
-  def row(line: List[String]): Row = {
-
-    Row((line.init.map(_.toString) :+ line.last.toInt): _*)
-
-    //or
-    //Row.fromSeq(line.init.map(_.toString) :+ line.last.toInt)
-  }
-
-
-
-  def philosopherSummaryTyped(philosopherSummaryDf: DataFrame): Dataset[PhilosopherRow] =
-    philosopherSummaryDf.as[PhilosopherRow]
-
-
-  def countrySummaryTyped(philosopherSummaryDf: DataFrame): Dataset[CountryRow] =
-    philosopherSummaryDf.select("country", "rating").as[CountryRow]
-
-  def descriptiveTyped(philosopherSummaryDf: DataFrame): Dataset[Descriptive] =
-    philosopherSummaryDf.select("country", "rating")
-      .withColumn("minRating", 'rating)
-      .withColumn("maxRating", 'rating)
-      .withColumn("range", 'rating)
-      .as[Descriptive]
-
-
-
-  def ratingsByCountry(df: DataFrame): Dataset[CountryRow] = {
+  def ratingsByCountry(ds: Dataset[CountryRow]): Dataset[CountryRow] = {
 
     /**DataFrame to DataSet
       *
       * Remember to import implicits outside scope of object of case class
       * Remember a ds is types so you must gives types to results with .as[type]
       */
-    val ds = philosopherSummaryTyped(df)
 
     ds
       .groupBy('country)
@@ -195,7 +128,7 @@ object Philosophers {
       ).as[CountryRow]
   }
 
-  def bestPhilosopherByCountry(df: DataFrame ): Dataset[CountryRow] = {
+  def bestPhilosopherByCountry(ds: Dataset[CountryRow] ): Dataset[CountryRow] = {
 
     /**DataFrame to DataSet
       *
@@ -203,7 +136,6 @@ object Philosophers {
       * Remember a ds is types so you must gives types to results with .as[type]
       */
 
-    val ds = philosopherSummaryTyped(df)
 
     ds
       .groupBy('country)
@@ -212,15 +144,13 @@ object Philosophers {
       ).as[CountryRow]
   }
 
-  def descPhilosopherByCountry(df: DataFrame): Dataset[Descriptive] = {
+  def descPhilosopherByCountry(ds: Dataset[Descriptive]): Dataset[Descriptive] = {
 
     /**DataFrame to DataSet
       *
       * Remember to import implicits outside scope of object of case class
       * Remember a ds is types so you must gives types to results with .as[type]
       */
-
-    val ds = descriptiveTyped(df)
 
     ds
       .groupBy('country)
